d2cf9d052e25126c2f3b4769f8538011950acd24
Richard <124844688+Ninju4821@users.noreply.github.com>
2023-06-13 11:55:29 -0400

    Fixed loading current single to the table.

--- timer.js
+++ timer.js
@@ -8,7 +8,7 @@ let holdStart = null;
 let timeStart = null;
 let timePenalty = 0;
 let dnf = false;
-let elapsed = 1;
+let elapsed = 0;
 let tableTimes = []; //TODO: Keep track of plus twos in the array (and cookie)
 let timeModifiers = [];
 //[session mean, mo3, ao5, ao12]
@@ -186,7 +186,7 @@ function UpdateAverages () {
   document.getElementById("meanTable").innerHTML = "<tr><th>Session Mean:</th></tr><tr><td>" + formatTime(sessionMean) + "</td></tr>";
   document.getElementById("sessionTable").innerHTML = "<tr><th></th><th>Current:</th><th>Best:</th></tr>"
   + "<tr><th>Single:</th>" 
-    + "<td>" + formatTime(elapsed) + "</td>" 
+    + "<td>" + formatTime(elapsed != 0 ? elapsed : tableTimes[0]) + "</td>" 
     + "<td>" + formatTime(bestTimes[0]) + "</td>"
   + "</tr>"
   + "<tr>"
